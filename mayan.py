9c2171d0847e836839c97af3bd3c7bc88dc16ae9
Ethan Luna <ethanlunacado@gmail.com>
2024-06-03 12:52:17 -0600

    added the expected results 4 each test

--- mayan.py
+++ mayan.py
@@ -66,28 +66,188 @@ def mayanTokenizer(sentence):
 sentences = [
             #I am drinking water regularly
             'Tene_ kin ukic já',
+            #Expected result:
+            """
+                S
+            ____|____
+            |        DET
+            |     ____|________
+            |   PMPV  |   V    |
+            |    |    |   |    |
+            PP   P   MPV  IV   N
+            |    |    |   |    |
+            tene_  k    in ukic  já
+            """
+
             #You are eating food regularly
             'Teche_ ka hantic janal',
+            #Expected result:
+            """
+                    S
+            _____|____
+            |         DET
+            |      ____|___________
+            |    PMPV  |    V      |
+            |     |    |    |      |
+            PP    P   MPV   IV     N
+            |     |    |    |      |
+            teche_  k    a  hantic janal
+            """
+
             #He/She is stretching the rope regularly
             'Leti_e_ ku haylic summ',
+            #Expected result:
+            """
+                    S
+                _____|____
+            |         DET
+            |      ____|__________
+            |    PMPV  |    V     |
+            |     |    |    |     |
+            PP    P   MPV   IV    N
+            |     |    |    |     |
+            leti_e_  k    u  haylic summ
+            """
+            
             #We are lifting the stone
             'Tone_ táan k naaczik tunich',
+            #Expected result:
+            """
+                    S
+                ____|____
+                |        DET
+                |     ____|____________
+                |   PMPV  |     V      |
+                |    |    |     |      |
+                PP   AV  MPV    IV     N
+                |    |    |     |      |
+                tone_ táan  k  naaczik tunich
+            """
+            
             #They are killing fish
             'Te_exe_ táan a cimzik kay',
+            #Expected result:
+            """
+                S
+            _____|____
+            |         DET
+            |      ____|__________
+            |    PMPV  |    V     |
+            |     |    |    |     |
+            PP    AV  MPV   IV    N
+            |     |    |    |     |
+            te_exe_ táan  a  cimzik kay
+            """
+
+            
             #They are learning python
             'Leti_obe_ ku canic python',
+            #Expected result:
+            """
+                  S
+            ______|____
+            |          DET
+            |       ____|__________
+            |     PMPV  |    V     |
+            |      |    |    |     |
+            PP     P   MPV   IV    N
+            |      |    |    |     |
+        leti_obe_  k    u  canic python
+
+"""
+
+            
             #I drink water regularly
             'Tene_ kin ukul já',
-            #you eat food regularly
+            #Expected result:
+            """
+                S
+            ____|____
+            |        DET
+            |     ____|________
+            |   PMPV  |   V    |
+            |    |    |   |    |
+            PP   P   MPV  TV   N
+            |    |    |   |    |
+            tene_  k    in ukul  já
+            """
+            
+            #You eat food regularly
             'Teche_ ka hanal janal',
+            #Expected result:
+            """
+                    S
+            _____|____
+            |         DET
+            |      ____|__________
+            |    PMPV  |    V     |
+            |     |    |    |     |
+            PP    P   MPV   TV    N
+            |     |    |    |     |
+            teche_  k    a  hanal janal
+            """
+
             #He/She stretch the rope regularly
             'Leti_e_ ku hayal summ',
+            #Expected result:
+            """
+                    S
+                _____|____
+            |         DET
+            |      ____|_________
+            |    PMPV  |    V    |
+            |     |    |    |    |
+            PP    P   MPV   TV   N
+            |     |    |    |    |
+            leti_e_  k    u  hayal summ
+            """
+            
             #We lift the stone
             'Tone_ táan k naacal tunich',
+            #Expected result:
+            """
+                S
+            ____|____
+            |        DET
+            |     ____|___________
+            |   PMPV  |    V      |
+            |    |    |    |      |
+            PP   AV  MPV   TV     N
+            |    |    |    |      |
+            tone_ táan  k  naacal tunich
+            """
+            
             #They kill fish
             'Te_exe_ táan a cimil kay',
+            #Expected result:
+            """
+                 S
+            _____|____
+            |         DET
+            |      ____|_________
+            |    PMPV  |    V    |
+            |     |    |    |    |
+            PP    AV  MPV   TV   N
+            |     |    |    |    |
+            te_exe_ táan  a  cimil kay
+            """
+
+            
             #They learn python
             'Leti_obe_ ku canal python',
+            #Expected result:
+            """
+                  S
+            ______|____
+            |          DET
+            |       ____|__________
+            |     PMPV  |    V     |
+            |      |    |    |     |
+            PP     P   MPV   TV    N
+            |      |    |    |     |
+        leti_obe_  k    u  canal python
+            """
+
             ]
 
 for sentence in sentences:
